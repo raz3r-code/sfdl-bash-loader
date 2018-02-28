816cf5b0b73da77c3bced609d96a1a4bcf794eba
raz3r-code <35331701+raz3r-code@users.noreply.github.com>
2018-02-28 21:50:25 +0100

    Update routine

--- sfdl_bash_loader/start.sh
+++ sfdl_bash_loader/start.sh
@@ -11,6 +11,19 @@
 # 888   d88P d8888888888 Y88b  d88P 888    888        888     Y88..88P 888  888 Y88b 888 Y8b.     888     
 # 8888888P" d88P     888  "Y8888P"  888    888        88888888 "Y88P"  "Y888888  "Y88888  "Y8888  888      
 # ==========================================================================================================
+version_repo=0
+version_local=0
+url_repoversion="https://raw.githubusercontent.com/raz3r-code/sfdl-bash-loader/master/sfdl_bash_loader/sys/logs/version.txt"
+
+checkupdate()
+{
+version_repo=$(wget -q -O - "$@" $url_repoversion | cut -d"." -f2)
+version_local=$(cat "./sys/logs/version.txt" | cut -d"." -f2)
+
+if [ $(($version_local)) -lt $(($version_repo)) ]; then
+	exec "$pwd/update.sh"
+fi
+}
 
 status=`ps aux | grep [-i] 'bashloader.sh' 2> /dev/null | wc -l | tr -d '[[:space:]]'`
 if [ "$status" -gt 0 ]; then
@@ -32,9 +45,11 @@ else
 fi
 
 if [ -f "$pwd/sys/setup.txt" ]; then
+	checkupdate
 	exec "$pwd/sys/bashloader.sh"
 	exit 0
 fi
+
 
 # macht das bild sauber
 clear
@@ -43,6 +58,8 @@ echo "| -------------------------------------- "
 echo "| BASH-Loader Installer"
 echo "| -------------------------------------- "
 echo "| system: $osxcheck"
+
+
 
 # ==========================================================================================================
 # haben wir alle tools?
@@ -323,9 +340,10 @@ if [ "${#installTools[@]}" != 0 ]; then
                                 echo "| File are not executable or found"
                                 chmod +x -R "$pwd/sys"
                                 echo "| Files are now executable"
-                fi
+            fi
 		echo "| Starte BASH-Loader in 5 Sekunden ..."
-                echo 1 > "$pwd/sys/setup.txt"
+        echo 1 > "$pwd/sys/setup.txt"
+		checkupdate
 		sleep 5
 		exec "$pwd/sys/bashloader.sh"
 	fi
@@ -343,6 +361,7 @@ else
                 fi
                 echo "| Starte BASH-Loader in 5 Sekunden ..."
                 echo 1 > "$pwd/sys/setup.txt"
-		sleep 5
+				checkupdate
+				sleep 5
                 exec "$pwd/sys/bashloader.sh"
 fi
