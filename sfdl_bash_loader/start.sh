296356f301bb8b36ca9cac526135493e3847c6a1
JobbeDeluxe <jobbedeluxe@gmail.com>
2018-03-02 21:42:19 +0100

    Update start.sh
    pfad fehler

--- sfdl_bash_loader/start.sh
+++ sfdl_bash_loader/start.sh
@@ -29,7 +29,7 @@ fi
 
 version_repo=$(wget -q -O - "$@" $url_repoversion | cut -d"." -f2)
 
-if [ -f "$pwd/sys/log/version.txt" ]; then
+if [ -f "$pwd/sys/logs/version.txt" ]; then
 	version_local=$(cat "$pwd/sys/logs/version.txt" | cut -d"." -f2)
 else
 	sfdl_update=false
