b1adf2d313f75ac378a5156713aa401c46fe0b0d
JobbeDeluxe <jobbedeluxe@gmail.com>
2018-04-07 15:54:26 +0200

    Update bashloader.sh

--- sfdl_bash_loader/sys/bashloader.sh
+++ sfdl_bash_loader/sys/bashloader.sh
@@ -571,7 +571,7 @@ do
 					printErr "$ladesfdl kann mit Passwort $aes_pass nicht entschluesselt werden!"
 					printErr "$ladesfdl wird uebersprungen!"
 					mkdir -p "$sfdl_files"/error
-					mv "$sfdl" "$sfdl_files"/error/$ladesfd.sfdl
+					mv "$sfdl" "$sfdl_files"/error/$ladesfdl.sfdl
 					continue
 				else
 					if [ $addNewPass == "true" ]; then
